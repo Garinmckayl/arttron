89b5cc5b2a113e447184c53ad0556e51f2a6d649
nate <natnaelgetenew@gmail.com>
2021-10-14 00:15:11 +0300

    event feature

--- nft.py
+++ nft.py
@@ -1,4 +1,6 @@
 import os
+import pandas as pd
+from web3 import Web3
 import streamlit as st
 import requests, json
 
@@ -42,6 +44,37 @@ if endpoint == 'Assets':
             else:
                 st.image(asset['image_url'])
 
+if endpoint == 'Events':
+    collection = st.sidebar.text_input('Collection')
+    asset_contract_address = st.sidebar.text_input('Contract Address')
+    token_id = st.sidebar.text_input('Token ID')
+    event_type = st.sidebar.selectbox("Event Type", ['offer_entered', 'cancelled', 'bid_withdrawn', 'transfer', 'approve'])
+    params = {}
+    if collection:
+        params['collection_slug'] = collection
+    if asset_contract_address:
+        params['asset_contract_address'] = asset_contract_address
+    if token_id:
+        params[token_id] = token_id
+    if event_type:
+        params['event_type'] = event_type
 
-    # r = requests.get("https://api.opensea.io/api/v1/assets", params=params)
+    r = requests.get("https://api.opensea.io/api/v1/events", params=params)
+    
+    events = r.json()
+    event_list = []
+    for event in events['asset_events']:
+        if event_type == 'offer_entered':
+            if event['bid_amount']:
+                bid_amount = Web3.fromWei(int(event['bid_amount']), 'ether')
+            if event['from_account']['user']:
+                bidder = event['from_account']['user']['username']
+            else:
+                bidder = event['from_account']['address']
+
+            event_list.append([event['created_date'], bidder, float(bid_amount), event['asset']['collection']['name'], event['asset']['token_id']])
+    
+    df = pd.DataFrame(event_list, columns=['time', 'bidder', 'bid_amount', 'collection', 'token_id'])
+    # st.subheader('Raw JSON Data')
+    st.write(df)
     # st.write(r.json())
